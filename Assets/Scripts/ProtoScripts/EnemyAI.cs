d3e3185327cd0f7a2dea276fa7831c4d4d86f353
Satu Remes <r3messa@gmail.com>
2020-03-17 17:42:33 +0200

    Chasing enemy added

---
+++ Assets/Scripts/ProtoScripts/EnemyAI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    NavMeshAgent NavMeshAgent;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        NavMeshAgent = GetComponent<NavMeshAgent>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        NavMeshAgent.SetDestination(target.position);
+
+    }
+}
