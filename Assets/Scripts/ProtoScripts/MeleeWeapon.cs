2baee193b5ecb51ef815deb760f9474e43749a61
Satu Remes <r3messa@gmail.com>
2020-03-20 20:28:44 +0200

    Enemy has health now and the player causes damage. Enemy can be destroyed.

---
+++ Assets/Scripts/ProtoScripts/MeleeWeapon.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeWeapon : MonoBehaviour
+{
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float range = 1f;
+    [SerializeField] float damage = 50f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
+            Debug.Log("Kuole saatanan " + hit.transform.name);
+            //TODO: add some hit effect for visual players
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            //method on EnemyHealth that decreases the enemy's health
+            target.TakeDamage(damage);
+        }
+        else
+        {
+            return;
+        }
+    }
+}
+
---
+++ Assets/Scripts/ProtoScripts/Weapon.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float range = 100f;
+    [SerializeField] float damage = 25f;
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        } 
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
+            Debug.Log("Kuole saatanan " + hit.transform.name);
+            //TODO: add some hit effect for visual players
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            // call a method on EnemyHealth that decreases the enemy's health
+        }
+        else
+        {
+            return;
+        }
+    }
+}
